6e738be78331d99f691fdd0e71d2a7966529143b
mrsekut <k.cloudspider@gmail.com>
2026-02-14 11:45:55 +0900

    refactor: remove GitHub-related features

--- src/features/file/atoms.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'jotai';
-import type { FileItem } from '../github/api.js';
-
-export const filesAtom = atom<FileItem[]>([]);
-export const selectedFileAtom = atom<FileItem | null>(null);
--- src/features/repository/atoms.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'jotai';
-import type { Repository } from '../github/api.js';
-
-export const repositoriesAtom = atom<Repository[]>([]);
-export const selectedRepositoryAtom = atom<Repository | null>(null);
